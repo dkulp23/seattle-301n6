47f4b7c669ca042994211854727a6b3e1cf397db
Dana Kulp <dkulp23@gmail.com>
2016-12-19 22:18:24 -0800

    Lecture notes about REST api and recursion.

---
+++ class-12-REST-and-APIs/lecture/public/app.js
@@ -0,0 +1,12 @@
+$.ajax({
+  url: 'https://api.github.com/user/repos?type=owner',
+  method: 'GET',
+  headers: {
+    Authorization: 'token ${githubToken}'
+  }
+})
+.then(data => {
+  data.forEach(repo => $('#results').append(`<p>${repo.name}</p>`))
+}, err => {
+  console.log('error: ' + err);
+})
